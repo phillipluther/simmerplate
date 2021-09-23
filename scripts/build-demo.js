cee6f65beb649783bd33cf98b6929e02b993275a
Phillip Luther <dev@phillipluther.com>
2021-09-23 06:52:10 -0700

    Feature: add build process for demo site

---
+++ scripts/build-demo.js
@@ -0,0 +1,24 @@
+const nunjucks = require('nunjucks');
+const path = require('path');
+const fs = require('fs');
+
+const srcPath = path.resolve('site');
+const destPath = path.resolve('public');
+
+nunjucks.configure(srcPath, {
+  autoescape: true,
+  trimBlocks: true,
+});
+
+if (fs.existsSync(destPath)) {
+  fs.rmSync(destPath, { recursive: true });
+}
+fs.mkdirSync(destPath);
+
+fs.readdirSync(srcPath).forEach((filename) => {
+  const rendered = nunjucks.render(filename);
+  fs.writeFileSync(
+    path.join(destPath, filename.replace(/\.njk$/, '.html')),
+    rendered,
+  );
+});
