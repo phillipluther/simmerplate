b1467eb820593fd7f58b0c99f5e16133acaca852
Phillip Luther <dev@phillipluther.com>
2021-09-29 07:07:19 -0700

    Update + simplify dev server to use HTML

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -2,25 +2,32 @@ const express = require('express');
 const livereload = require('livereload');
 const connectLivereload = require('connect-livereload');
 const path = require('path');
-
-const publicPath = path.join(__dirname, '../public');
+const fs = require('fs');
+const { STYLES_DEST_DIR, SITE_SRC_DIR } = require('./constants');
 
 const app = express();
 app.use(connectLivereload());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(publicPath));
+app.use(express.static(SITE_SRC_DIR));
 
 const livereloadServer = livereload.createServer();
-livereloadServer.watch(publicPath);
+livereloadServer.watch([SITE_SRC_DIR, STYLES_DEST_DIR]);
 
-app.get('/:page', (req, res) => {
-  const { page = 'index' } = req.params;
+app.get('/:file', (req, res) => {
+  const { file = 'index' } = req.params;
+  
+  if (/\.css$/.test(file)) {
+    res.sendFile(path.join(STYLES_DEST_DIR, file));
+    return;
+  } 
 
-  try {
-    res.sendFile(path.join(publicPath, `${page}.html`));
-  } catch (e) {
-    console.error(e);
+  const htmlFile = path.join(SITE_SRC_DIR, `${file}.html`);
+  if (fs.existsSync(htmlFile)) {
+    res.sendFile(htmlFile);
+    return;
   }
+
+  res.send(404);
 });
 
 app.listen(8000, () => {
